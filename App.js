e1680b553c4781108cc1bfa971733ac9aa610a11
Kevin Machava <kevinmachava20@gmail.com>
2025-04-13 11:36:19 +0200

    Onebitcoin configurations

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, StatusBar, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+     
+      <StatusBar
+        backgroundColor='#D4af37'
+        barStyle="dark-content" // ou light-content
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? 40: 0,
+    
   },
 });
---
+++ src/components/currentPrice/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, Text} from 'react-native'
+import style from "./style"
+
+export default function CurrentPrice(){
+    return(
+        <View>
+            <Text>
+                $ 54423.365
+            </Text>
+            <Text>
+                Ultima cotacao
+            </Text>
+        </View>
+    )
+}
